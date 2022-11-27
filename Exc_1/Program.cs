e45624543a29a3df15960b6e718b05928b0bdd0d
Slava <slavanew98@mail.ru>
2022-11-27 15:46:19 +0300

    1st exercise - completed

--- Exc_1/Program.cs
+++ Exc_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите 3х-значное число: ");
+
+bool isParse = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParse)
+{
+    Console.WriteLine("Введено не правильное значение");
+}
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Введено не правильное значение");
+}
+
+int second_num = num % 100;
+second_num = second_num / 10;
+
+Console.WriteLine($"Вторая цифра числа: {second_num}");
