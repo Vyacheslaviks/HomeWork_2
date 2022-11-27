811c58049518ffc3dbde2d74af0ca0952d043b20
Slava <slavanew98@mail.ru>
2022-11-27 16:23:44 +0300

    3rd exercise - completed

--- Exc_3/Program.cs
+++ Exc_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.Console.Write("Введите число: ");
+
+Console.Write("Введите номер дня недели: ");
+
+bool isParse = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParse)
+{
+    Console.WriteLine("Введено не правильное значение");
+}
+
+if (num > 0 && num < 8)
+{
+    if (num != 6 && num != 7)
+        Console.WriteLine("НЕ выходной, марш работать");
+    else
+        Console.WriteLine("Выходной!!!");
+}
+else
+{
+    Console.WriteLine("Нет такого дня недели");
+}
