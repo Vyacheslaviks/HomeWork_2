1204333472340592657e17e94d599191d6f179eb
Slava <slavanew98@mail.ru>
2022-11-27 16:07:37 +0300

    2nd exercise - completed

--- Exc_2/Program.cs
+++ Exc_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+
+bool isParse = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParse)
+{
+    Console.WriteLine("Введено не правильное значение");
+}
+
+if (num > 99)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine("Число меньше трезначного");
+}
